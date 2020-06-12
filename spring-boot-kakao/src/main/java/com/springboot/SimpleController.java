874b212d32bacf09d1ebf1f4bb46eb239ac2895f
kimtg <kimtg@DESKTOP-KB2D366>
2020-06-12 10:46:19 +0900

    20200612 kimtg kakao test complete

--- spring-boot-kakao/src/main/java/com/springboot/SimpleController.java
+++ spring-boot-kakao/src/main/java/com/springboot/SimpleController.java
@@ -1,16 +1,12 @@
 package com.springboot;
 
 import java.util.ArrayList;
-import java.util.List;
 
-import javax.annotation.Resource;
-
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.*;
 
-import com.springboot.board.service.BoardService;
-import com.springboot.board.mapper.BoardMapper;
+
 
 /**
  * Copyright 2020 by kimtg ALL right reserved.
@@ -21,35 +17,123 @@ import com.springboot.board.mapper.BoardMapper;
  * @description
  * </pre>
  */
-@Controller
+@RestController
 public class SimpleController {
-
-	@Resource
-	private BoardService boardService;
 
 
 	@RequestMapping("/test")
-	public ModelAndView test() throws Exception{
-		ModelAndView mav = new ModelAndView("test");
-		mav.addObject("name", "태건아 안녕?");
-		List<String> testList = new ArrayList<String>();
-		testList.add("a");
-		testList.add("b");
-		testList.add("c");
-		mav.addObject("list", testList);
-		return mav;
-	}
+    private String jspTest(){
+        System.out.println("Hello world~");
+        return "hello world~";
+    }
 
-    @RequestMapping("/test2")
-    private String jspTest() throws Exception{
+	// 버튼 키보드
+    @RequestMapping(value = "/keyboard", method = RequestMethod.GET)
+    public String keyboard() {
 
-    	int boardCount = boardService.getBoardCount();
-        System.out.println(boardCount);
+        System.out.println("/keyboard");
 
-        return "test";
+        JSONObject jobjBtn = new JSONObject();
+        ArrayList<String> btns = new ArrayList<>();
+        btns.add("신청");
+        btns.add("나의 신청 내역 (고객센터)");
+
+        jobjBtn.put("type", "buttons");
+        jobjBtn.put("buttons",btns);
+
+        return jobjBtn.toJSONString();
+    }
+
+    // 단순 키보드
+    @RequestMapping(value = "/keyboard2", method = RequestMethod.GET)
+    public String keyboard2() {
+
+        System.out.println("/keyboard");
+
+        JSONObject jobjBtn = new JSONObject();
+        jobjBtn.put("type", "text");
+
+        return jobjBtn.toJSONString();
     }
 
 
+    //버튼메세지
+    @RequestMapping(value = "/message", method = RequestMethod.POST, headers = "Accept=application/json")
+    @ResponseBody
+    public String message(@RequestBody JSONObject resObj) {
+
+        System.out.println("/message");
+        System.out.println(resObj.toJSONString());
+
+        String content;
+        content = (String) resObj.get("content");
+        JSONObject jobjRes = new JSONObject();
+        JSONObject jobjText = new JSONObject();
+        JSONObject jobjmesBtn = new JSONObject();
+        JSONObject jobjBtn = new JSONObject();
+
+        ArrayList<String> btns = new ArrayList<>();
+        btns.add("신청");
+        btns.add("나의 신청 내역 (고객센터)");
+        jobjBtn.put("type", "buttons");
+        jobjBtn.put("buttons", btns);
+
+        if(content.contains("신청")){
+            System.out.println("click A");
+            jobjText.put("text","안녕하세요 고객님! 신청해주셔서 감사합니다. 아래의 URL을 클릭하여 작성해주세요.");
+            jobjmesBtn.put("label","신청서 작성");
+            jobjmesBtn.put("url"," http://www.abc.com");
+            jobjText.put("message_button",jobjmesBtn);
+
+        } else if(content.contains("나의 신청 내역")){
+            System.out.println("click B");
+            jobjText.put("text","안녕하세요 고객님! 신청하신 내역은 아래의 URL에서 확인하실 수 있습니다");
+            jobjmesBtn.put("label","고객센터 문의");
+            jobjmesBtn.put("url"," http://www.abc.com");
+            jobjText.put("message_button",jobjmesBtn);
+
+        }
+
+        jobjRes.put("message", jobjText);
+        jobjRes.put("keyboard", jobjBtn);
+        System.out.println(jobjRes.toJSONString());
+
+        return  jobjRes.toJSONString();
+    }
+
+    //단순 메세지
+    @RequestMapping(value = "/message2", method = RequestMethod.POST, headers = "Accept=application/json")
+    @ResponseBody
+    public String message2(@RequestBody JSONObject resObj) {
+
+        System.out.println("/message");
+        System.out.println(resObj.toJSONString());
+
+        String content;
+        content = (String) resObj.get("content");
+        JSONObject jobjRes = new JSONObject();
+        JSONObject jobjText = new JSONObject();
+
+        // 사용자 구현
+        if(content.contains("안녕")){
+            jobjText.put("text","안녕 하세요");
+        } else if(content.contains("사랑")){
+            jobjText.put("text","나도 너무너무 사랑해");
+        } else if(content.contains("잘자")){
+            jobjText.put("text","꿈 속에서도 너를 볼꺼야");
+        } else if(content.contains("졸려")){
+            jobjText.put("text","졸리면 언능 세수하러 가용!");
+        } else if(content.contains("시간")||content.contains("몇 시")){
+            jobjText.put("text","섹시");
+        } else {
+            jobjText.put("text","흠... 아직 지정해 두지 않은 말인걸.");
+        }
+
+        jobjRes.put("message", jobjText);
+        System.out.println(jobjRes.toJSONString());
+
+        return  jobjRes.toJSONString();
+    }
 
 
 
